9525e8da699f829a067f9fbbe475dcc960422533
Maksim Gruzdev <mgruzdev@cheby.ru>
2025-08-11 23:11:57 +0300

    mv model url to conf

--- back/lib/Content/chat.js
+++ back/lib/Content/chat.js
@@ -8,7 +8,7 @@ const openai2chad = async function (rq) {
     }
 }
 
-const chad2openai = async function (rp) {
+const chad2openai = async function (rp, model) {
     if (!rp.is_success) {
         return {
             error: {
@@ -22,10 +22,10 @@ const chad2openai = async function (rp) {
 
     const created = Math.floor(new Date ().getTime() / 1000)
     return {
-    "id": `chatcmpl-${created}`,
+    "id": `cmpl-${created}`,
     "object": "chat.completion",
     created,
-    "model": "gpt-4o",
+    "model": model,
     "choices": [
       {
         "message": {
@@ -33,13 +33,14 @@ const chad2openai = async function (rp) {
           "content": rp.response
         },
         "index": 0,
+        "logprobs": null,
         "finish_reason": "stop"
       }
     ],
     "usage": {
-      "prompt_tokens": rp.used_tokens_count,
-      "completion_tokens": rp.used_tokens_count,
-      "total_tokens": rp.used_tokens_count
+      "prompt_tokens": rp.used_words_count,
+      "completion_tokens": rp.used_words_count,
+      "total_tokens": rp.used_words_count
     }
   }
 }
@@ -53,10 +54,17 @@ allowAnonymous: true,
 doCompletions:
 
     async function () {
+        const headers = this.http.request.headers
+        const authorization = headers.authorization
+        const api_key = authorization? authorization.split (' ')[1]: this.conf.CHAD_TOKEN
+        console.log ({headers})
+
+        if (!api_key) throw new Error ('CHAD_TOKEN is not defined')
+
         const rqChad = await openai2chad(this.request)
-        const api_key = CHAD_TOKEN
         const rq = {...rqChad, api_key}
-        const rp_raw = await fetch(`https://ask.chadgpt.ru/api/public/gpt-4o-mini`, {
+        const {model, url} = this.conf
+        const rp_raw = await fetch(`${url}/${model}`, {
             method: 'POST',
             body: JSON.stringify (rq),
             headers: {
@@ -65,7 +73,7 @@ doCompletions:
         })
         const rp_json = await rp_raw.json ()
 
-        const rp_ai = await chad2openai (rp_json)
+        const rp_ai = await chad2openai (rp_json, model)
         console.log ({rq, rp_json, rp_ai})
         return rp_ai
     },
